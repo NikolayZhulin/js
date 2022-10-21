bdcecde9b60c81b0650d76f21462c74fa7efc614
NikolayZhulin <nikolayzhulin97@mail.ru>
2022-10-21 15:13:12 +0300

    added new task

--- script.js
+++ script.js
@@ -16,31 +16,60 @@
 
 // console.log(boolToWord(true));
 
-let number = prompt("Vvedite chislo");
+// let number = prompt("Vvedite chislo");
 
-if (number > 0) {
-  alert("1");
-} else if (number == 0) {
-  alert("0");
-} else if (number < 0) {
-  alert("-1");
-}
+// if (number > 0) {
+//   alert("1");
+// } else if (number == 0) {
+//   alert("0");
+// } else if (number < 0) {
+//   alert("-1");
+// }
 
-let nameJS = prompt("Kakoe officialnoe nazvanie js");
+// let nameJS = prompt("Kakoe officialnoe nazvanie js");
 
-if (nameJS == "ecmascript") {
-  alert("Verno");
+// if (nameJS == "ecmascript") {
+//   alert("Verno");
+// } else {
+//   alert("ne znaete?");
+// }
+
+// let result = a + b < 4 ? "Malo" : "Mnogo";
+
+// let message =
+//   login == "sotrudnik"
+//     ? "Privet"
+//     : login == "Director"
+//     ? "Zdravstvuite"
+//     : login == ""
+//     ? "Net logina"
+//     : "";
+
+// if (age >= 14 && age <= 90) {
+//   return true;
+// }
+
+// if (age < 14 || age > 90) {
+//   return true;
+// }
+
+// if (!(age >= 14 && age <= 90)) {
+//   return true;
+// }
+
+let login = prompt("Who here?");
+
+if (login == "Admin") {
+  let password = prompt("password");
+  if (password === "I am main") {
+    alert("hello");
+  } else if (password === null || password === "") {
+    alert("cansel");
+  } else {
+    alert("Invalid password");
+  }
+} else if (login === null || login === "") {
+  alert("cansel");
 } else {
-  alert("ne znaete?");
+  alert("i dont know you");
 }
-
-let result = a + b < 4 ? "Malo" : "Mnogo";
-
-let message =
-  login == "sotrudnik"
-    ? "Privet"
-    : login == "Director"
-    ? "Zdravstvuite"
-    : login == ""
-    ? "Net logina"
-    : "";
