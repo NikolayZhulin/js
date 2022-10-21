20c665fb2b7c90fe09f73689e84d5789d4b99c11
NikolayZhulin <nikolayzhulin97@mail.ru>
2022-10-21 14:20:04 +0300

    new tasks

--- script.js
+++ script.js
@@ -9,9 +9,27 @@
 //   return "hello world!";
 // }
 
-function boolToWord(bool) {
-  if (bool == false) return "No";
-  if (bool == true) return "Yes";
+// function boolToWord(bool) {
+//   if (bool == false) return "No";
+//   if (bool == true) return "Yes";
+// }
+
+// console.log(boolToWord(true));
+
+let number = prompt("Vvedite chislo");
+
+if (number > 0) {
+  alert("1");
+} else if (number == 0) {
+  alert("0");
+} else if (number < 0) {
+  alert("-1");
 }
 
-console.log(boolToWord(true));
+let nameJS = prompt("Kakoe officialnoe nazvanie js");
+
+if (nameJS == "ecmascript") {
+  alert("Verno");
+} else {
+  alert("ne znaete?");
+}
