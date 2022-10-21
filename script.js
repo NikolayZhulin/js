deb24f473b7d209de40f27dedb2add514f15379f
NikolayZhulin <nikolayzhulin97@mail.ru>
2022-10-21 21:01:29 +0300

    added tasks

--- script.js
+++ script.js
@@ -79,8 +79,72 @@
 //   console.log(i);
 // }
 
-let i = 0;
-while (i < 3) {
-  console.log(`number ${i}`);
-  i++;
+// let i = 0;
+// while (i < 3) {
+//   console.log(`number ${i}`);
+//   i++;
+// }
+
+// while (+prompt("enter number") <= 100) {}
+
+// let n = 10;
+
+// for (i = 1; i <= n; i++) {
+//   for (j = 2; j <= i; j++) {
+//     if (i % j == 0 && j < i) break;
+//     else if (j === i) {
+//       console.log(i);
+//     }
+//   }
+// }
+
+// if (browser == "Edge") {
+//   console.log("You've got the edge");
+// } else if (
+//   browser == "Chrome" ||
+//   browser == "Firefox" ||
+//   browser == "Safari" ||
+//   browser == "Opera"
+// ) {
+//   console.log("Okay we support these browsers too");
+// } else {
+//   alert("We hope that this page looks ok!");
+// }
+
+// const number = +prompt("Введите число между 0 и 3", "");
+// switch (number) {
+//   case 0:
+//     console.log("0");
+//     break;
+
+//   case 1:
+//     console.log("1");
+//     break;
+//   case 2:
+//   case 3:
+//     console.log("2 or 3");
+//     break;
+// }
+
+// function checkAge(age) {
+//   if (age > 18) {
+//     return true;
+//   } else {
+//     return confirm("Родители разрешили?");
+//   }
+// }
+
+// function checkAge(age) {
+//   age > 18 ? true : confirm("Родители разрешили?");
+// }
+
+// function min(a, b) {
+//   if (a < b) return a;
+//   if (a > b) return b;
+// }
+
+function pow(a, b) {
+  if (a % 1 == 0 && b % 1 == 0) return a ** b;
 }
+
+console.log(pow(1, 3));
