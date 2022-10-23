fc0321a16c671ddfa305545866698bcf5b9e22a9
NikolayZhulin <nikolayzhulin97@mail.ru>
2022-10-23 20:57:15 +0300

    added tasks

--- script.js
+++ script.js
@@ -1,270 +1,278 @@
-// function solution(str, ending) {
-//   if (str == ending) return true;
-//   return str.endsWith(ending);
-// }
+function solution(str, ending) {
+  if (str == ending) return true;
+  return str.endsWith(ending);
+}
 
-// console.log(solution("str", "tr"));
+console.log(solution("str", "tr"));
 
-// function greet() {
-//   return "hello world!";
-// }
+function greet() {
+  return "hello world!";
+}
 
-// function boolToWord(bool) {
-//   if (bool == false) return "No";
-//   if (bool == true) return "Yes";
-// }
+function boolToWord(bool) {
+  if (bool == false) return "No";
+  if (bool == true) return "Yes";
+}
 
-// console.log(boolToWord(true));
+console.log(boolToWord(true));
 
-// let number = prompt("Vvedite chislo");
+let number = prompt("Vvedite chislo");
 
-// if (number > 0) {
-//   alert("1");
-// } else if (number == 0) {
-//   alert("0");
-// } else if (number < 0) {
-//   alert("-1");
-// }
+if (number > 0) {
+  alert("1");
+} else if (number == 0) {
+  alert("0");
+} else if (number < 0) {
+  alert("-1");
+}
 
-// let nameJS = prompt("Kakoe officialnoe nazvanie js");
+let nameJS = prompt("Kakoe officialnoe nazvanie js");
 
-// if (nameJS == "ecmascript") {
-//   alert("Verno");
-// } else {
-//   alert("ne znaete?");
-// }
+if (nameJS == "ecmascript") {
+  alert("Verno");
+} else {
+  alert("ne znaete?");
+}
 
-// let result = a + b < 4 ? "Malo" : "Mnogo";
+let result = a + b < 4 ? "Malo" : "Mnogo";
 
-// let message =
-//   login == "sotrudnik"
-//     ? "Privet"
-//     : login == "Director"
-//     ? "Zdravstvuite"
-//     : login == ""
-//     ? "Net logina"
-//     : "";
+let message =
+  login == "sotrudnik"
+    ? "Privet"
+    : login == "Director"
+    ? "Zdravstvuite"
+    : login == ""
+    ? "Net logina"
+    : "";
 
-// if (age >= 14 && age <= 90) {
-//   return true;
-// }
+if (age >= 14 && age <= 90) {
+  return true;
+}
 
-// if (age < 14 || age > 90) {
-//   return true;
-// }
+if (age < 14 || age > 90) {
+  return true;
+}
 
-// if (!(age >= 14 && age <= 90)) {
-//   return true;
-// }
+if (!(age >= 14 && age <= 90)) {
+  return true;
+}
 
-// let login = prompt("Who here?");
+let login = prompt("Who here?");
 
-// if (login == "Admin") {
-//   let password = prompt("password");
-//   if (password === "I am main") {
-//     alert("hello");
-//   } else if (password === null || password === "") {
-//     alert("cansel");
-//   } else {
-//     alert("Invalid password");
-//   }
-// } else if (login === null || login === "") {
-//   alert("cansel");
-// } else {
-//   alert("i dont know you");
-// }
+if (login == "Admin") {
+  let password = prompt("password");
+  if (password === "I am main") {
+    alert("hello");
+  } else if (password === null || password === "") {
+    alert("cansel");
+  } else {
+    alert("Invalid password");
+  }
+} else if (login === null || login === "") {
+  alert("cansel");
+} else {
+  alert("i dont know you");
+}
 
-// for (let i = 1; i <= 10; i++) {
-//   if (i % 2 != 0) continue;
-//   console.log(i);
-// }
+for (let i = 1; i <= 10; i++) {
+  if (i % 2 != 0) continue;
+  console.log(i);
+}
 
-// let i = 0;
-// while (i < 3) {
-//   console.log(`number ${i}`);
-//   i++;
-// }
+let i = 0;
+while (i < 3) {
+  console.log(`number ${i}`);
+  i++;
+}
 
-// while (+prompt("enter number") <= 100) {}
+while (+prompt("enter number") <= 100) {}
 
-// let n = 10;
+let n = 10;
 
-// for (i = 1; i <= n; i++) {
-//   for (j = 2; j <= i; j++) {
-//     if (i % j == 0 && j < i) break;
-//     else if (j === i) {
-//       console.log(i);
-//     }
-//   }
-// }
+for (i = 1; i <= n; i++) {
+  for (j = 2; j <= i; j++) {
+    if (i % j == 0 && j < i) break;
+    else if (j === i) {
+      console.log(i);
+    }
+  }
+}
 
-// if (browser == "Edge") {
-//   console.log("You've got the edge");
-// } else if (
-//   browser == "Chrome" ||
-//   browser == "Firefox" ||
-//   browser == "Safari" ||
-//   browser == "Opera"
-// ) {
-//   console.log("Okay we support these browsers too");
-// } else {
-//   alert("We hope that this page looks ok!");
-// }
+if (browser == "Edge") {
+  console.log("You've got the edge");
+} else if (
+  browser == "Chrome" ||
+  browser == "Firefox" ||
+  browser == "Safari" ||
+  browser == "Opera"
+) {
+  console.log("Okay we support these browsers too");
+} else {
+  alert("We hope that this page looks ok!");
+}
 
-// const number = +prompt("Введите число между 0 и 3", "");
-// switch (number) {
-//   case 0:
-//     console.log("0");
-//     break;
+const number = +prompt("Введите число между 0 и 3", "");
+switch (number) {
+  case 0:
+    console.log("0");
+    break;
 
-//   case 1:
-//     console.log("1");
-//     break;
-//   case 2:
-//   case 3:
-//     console.log("2 or 3");
-//     break;
-// }
+  case 1:
+    console.log("1");
+    break;
+  case 2:
+  case 3:
+    console.log("2 or 3");
+    break;
+}
 
-// function checkAge(age) {
-//   if (age > 18) {
-//     return true;
-//   } else {
-//     return confirm("Родители разрешили?");
-//   }
-// }
+function checkAge(age) {
+  if (age > 18) {
+    return true;
+  } else {
+    return confirm("Родители разрешили?");
+  }
+}
 
-// function checkAge(age) {
-//   age > 18 ? true : confirm("Родители разрешили?");
-// }
+function checkAge(age) {
+  age > 18 ? true : confirm("Родители разрешили?");
+}
 
-// function min(a, b) {
-//   if (a < b) return a;
-//   if (a > b) return b;
-// }
+function min(a, b) {
+  if (a < b) return a;
+  if (a > b) return b;
+}
 
-// function pow(a, b) {
-//   if (a % 1 == 0 && b % 1 == 0) return a ** b;
-// }
+function pow(a, b) {
+  if (a % 1 == 0 && b % 1 == 0) return a ** b;
+}
 
-// console.log(pow(1, 3));
+console.log(pow(1, 3));
 
-// function ask(question, yes, no) {
-//   if (confirm(question)) yes();
-//   else no();
-// }
+function ask(question, yes, no) {
+  if (confirm(question)) yes();
+  else no();
+}
 
-// ask(
-//   "Вы согласны?",
-//   () => alert("Вы согласились."),
-//   () => alert("Вы отменили выполнение.")
-// );
+ask(
+  "Вы согласны?",
+  () => alert("Вы согласились."),
+  () => alert("Вы отменили выполнение.")
+);
 
-// function makeNegative(num) {
-//   return -num;
-// }
+function makeNegative(num) {
+  return -num;
+}
 
-// console.log(makeNegative(9));
+console.log(makeNegative(9));
 
-// function check(a, x) {
-//   return a.includes(x);
-// }
+function check(a, x) {
+  return a.includes(x);
+}
 
-// function oddOrEven(array) {
-//   var sum = 0;
-//   for (var i = 0; i < array.length; i++) {
-//     sum += array[i];
-//   }
-//   if (sum % 2 == 0) {
-//     return "even";
-//   } else return "odd";
-// }
+function oddOrEven(array) {
+  var sum = 0;
+  for (var i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  if (sum % 2 == 0) {
+    return "even";
+  } else return "odd";
+}
 
-// function solution(str) {
-//   return str.split("").reverse().join("");
-// }
+function solution(str) {
+  return str.split("").reverse().join("");
+}
 
-// console.log(solution("world"));
+console.log(solution("world"));
 
-// function greet(name) {
-//   return `Hello, ${name} how are you doing today?`;
-// }
+function greet(name) {
+  return `Hello, ${name} how are you doing today?`;
+}
 
-// const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
-//   if (fuelLeft * mpg >= distanceToPump) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// };
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+  if (fuelLeft * mpg >= distanceToPump) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
-// console.log(zeroFuel(120, 25, 2));
+console.log(zeroFuel(120, 25, 2));
 
-// function paperwork(n, m) {
-//   if (n < 0 || m < 0) {
-//     return 0;
-//   } else return n * m;
-// }
+function paperwork(n, m) {
+  if (n < 0 || m < 0) {
+    return 0;
+  } else return n * m;
+}
 
-// console.log(paperwork(5, 5));
+console.log(paperwork(5, 5));
 
-// let arr = [];
-// let i = 1;
-// while (i <= 20) {
-//   if (i % 2 == 0) arr.push(i);
+let arr = [];
+let i = 1;
+while (i <= 20) {
+  if (i % 2 == 0) arr.push(i);
 
-//   i++;
-// }
-// console.log(arr);
+  i++;
+}
+console.log(arr);
 
-// for (let i = 30; i >= 1; i--) {
-//   if (i % 3 == 0) console.log(i);
-// }
+for (let i = 30; i >= 1; i--) {
+  if (i % 3 == 0) console.log(i);
+}
 
-// let arr = [];
-// for (let i = 0; i <= 10; i++) {
-//   console.log(i ** 2);
-// }
-// console.log(arr);
+let arr = [];
+for (let i = 0; i <= 10; i++) {
+  console.log(i ** 2);
+}
+console.log(arr);
 
-// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 16];
-// let count = 0;
-// for (i = 0; i < arr.length; i++) {
-//   if (arr[i] % 2 == 0) count++;
-// }
-// console.log(count);
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 16];
+let count = 0;
+for (i = 0; i < arr.length; i++) {
+  if (arr[i] % 2 == 0) count++;
+}
+console.log(count);
 
-// let str = "";
-// for (i = 0; i <= 10; i++) {
-//   console.log((str += "*"));
-// }
+let str = "";
+for (i = 0; i <= 10; i++) {
+  console.log((str += "*"));
+}
 
-// let count = 0;
-// let out = document.querySelector(".outer");
-// let flag = 3;
+let count = 0;
+let out = document.querySelector(".outer");
+let flag = 3;
 
-// while (count < 4) {
-//   let count1 = 0;
-//   while (count1 < 4) {
-//     if (count1 < flag) {
-//       out.innerHTML += "&nbsp";
-//     } else {
-//       out.innerHTML += "* ";
-//     }
-//     count1++;
-//   }
-//   flag--;
-//   count++;
-//   out.innerHTML += "<br>";
-// }
+while (count < 4) {
+  let count1 = 0;
+  while (count1 < 4) {
+    if (count1 < flag) {
+      out.innerHTML += "&nbsp";
+    } else {
+      out.innerHTML += "* ";
+    }
+    count1++;
+  }
+  flag--;
+  count++;
+  out.innerHTML += "<br>";
+}
 
-// function findAverage(array) {
-//     let summ = 0;
-//     for (i = 0; i < array.length; i++){
-//         arra
-//     }
-//     return 0;
-//   }
+function findAverage(array) {
+  let sum = 0;
+  let average;
+  if (array.length == 0) {
+    return 0;
+  } else {
+    for (i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+    console.log(sum);
+    return (average = sum / array.length);
+  }
+}
+
+console.log(findAverage([1, 2, 3, 4]));
 
 let location1 = 1;
 let location2 = 2;
@@ -276,7 +284,7 @@ let issunk = false;
 
 while (issunk === false) {
   guess = prompt("vvedi koordinati");
-  if (guess < 0 || guess > 6) {
+  if (guess <= 0 || guess > 6) {
     alert("nekorrektno");
   } else {
     guesses += 1;
@@ -291,3 +299,28 @@ while (issunk === false) {
     }
   }
 }
+
+function findShort(s) {
+  let str = s.split(" ");
+  let lengths = str.map((item) => item.length);
+  let min = lengths[0];
+  for (item of lengths) {
+    if (item < min) {
+      min = item;
+    }
+  }
+  return min;
+}
+
+console.log(findShort("bitcoin take over the world maybe who knows perhaps"));
+
+function abbrevName(name) {
+  let arr = name.split(" ");
+  const str = arr
+    .map((word) => word[0])
+    .join(".")
+    .toUpperCase();
+  return str;
+}
+
+console.log(abbrevName("Nikolay Zhulin"));
